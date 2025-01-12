4eba2e750af57ec760b4e025ccf163a2f785d794
lukriko <156338634+lukriko@users.noreply.github.com>
2025-01-13 01:25:58 +0400

    Delete sql directory

--- sql/top10.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM geo_movies
-LIMIT 5;
-
--- identify the movies or shows with the highest number of votes.
-SELECT "rating","votes","title" FROM geo_movies
-ORDER BY "votes" DESC
-LIMIT 10;
